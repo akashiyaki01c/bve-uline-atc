a1ae7a73dab88fbeef7683b6c2ec8721759959dc
akashiyaki01c <tanakahiroki.kobe@gmail.com>
2024-08-20 01:00:15 +0900

    update: ビルドスクリプトを更新

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo "Windows向けx86プラグインをコンパイルします。"
-cargo build --target=i686-pc-windows-gnu
+cargo build --target=i686-pc-windows-gnu --release
+
 echo "Windows向けx64プラグインをコンパイルします。"
-cargo build --target=x86_64-pc-windows-gnu
+cargo build --target=x86_64-pc-windows-gnu --release
+
+echo "出力物のコピーを行います。"
+cp target/x86_64-pc-windows-gnu/release/bve_kobecitysubway_ats.dll ./output/ULine_x64.dll
+cp target/i686-pc-windows-gnu/release/bve_kobecitysubway_ats.dll ./output/ULine_x86.dll
+
+echo "すべて終了しました。"
